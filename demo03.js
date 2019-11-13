45fde98e2cbe9ac69506ce7011cea93b78aa7e11
L.Rain <baixiaoyu@abrain.com.cn>
2019-11-13 22:38:18 +0800

    part5

--- demo03.js
+++ demo03.js
@@ -17,11 +17,38 @@ app.use(async(ctx)=>{
         `;
         ctx.body=html;
     }else if(ctx.url==='/'&&ctx.method==='POST'){
-        ctx.body='接收到POST参数'
+        let postData=await parsePostData(ctx)
+        ctx.body=postData
     }else{
         ctx.body='<h1>404!</h1>'
     }
 })
+function parsePostData(ctx){
+    return new Promise((resolve,reject)=>{
+        try{
+            let postdata=''
+            ctx.req.addListener('data',(data)=>{
+                postdata+=data;
+            })
+            ctx.req.on('end',function(){
+                resolve(parseQueryStr(postdata))
+            })
+        }catch(error){
+
+        }
+    })
+}
+function parseQueryStr(queryStr){
+    let queryData={};
+    let queryStrList=queryStr.split('&');
+    console.log(queryStrList)
+    for(let [index,queryStr] of queryStrList.entries()){
+        let itemList=queryStr.split('=')
+        queryData[itemList[0]]=decodeURIComponent(itemList[1])
+    }
+
+    return queryData
+}
 app.listen(3000,()=>{
     console.log('[demo is starting at port 3000]')
 })
